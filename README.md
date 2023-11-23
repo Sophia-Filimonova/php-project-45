54395cb30c7360ecce302275d52e03650c0f62b6
Sophia Filimonova <sophfil708@gmail.com>
2023-11-24 00:11:25 +0300

    add brain-calc

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Sophia-Filimonova/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Sophia-Filimonova/php-project-45/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/074ca46625cb98c2b80e/maintainability)](https://codeclimate.com/github/Sophia-Filimonova/php-project-45/maintainability)
+
+## Prerequisites
+
+* Linux, Macos, WSL
+* PHP >=8.1
+* Make
+* Git
+
+## Setup
+
+```bash
+git clone https://github.com/Sophia-Filimonova/php-project-45.git
+cd php-project-45 
+make install
+```
---
+++ src/Games/Calc.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Games\Calc;
+
+const RULES = 'What is the result of the expression?';
+
+function generateQuestion()
+{
+    $operators = ['+', '-', '*'];
+    $operator = $operators[array_rand($operators)];
+    $firstNumber = mt_rand(1, 50);
+    $secondNumber = mt_rand(1, 50);
+    switch ($operator) {
+        case '+':
+            $correctAnswer = $firstNumber + $secondNumber;
+            break;
+        case '-':
+            $correctAnswer = $firstNumber - $secondNumber;
+            break;
+        case '*':
+            $firstNumber = mt_rand(1, 25);
+            $secondNumber = mt_rand(1, 10);
+            $correctAnswer = $firstNumber * $secondNumber;
+
+    }
+    $question = "{$firstNumber} {$operator} {$secondNumber}";
+    return [
+        'correct answer' => (string) $correctAnswer,
+        'question' => $question
+    ];
+}
